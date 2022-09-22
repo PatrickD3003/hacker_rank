021e3d3eb758491356fab7613ba250119c3a1adb
PatrickD3003 <dharmapatrick88@gmail.com>
2022-09-22 22:14:46 +0900

    benerin anglembc program

--- angle_MBC.py
+++ angle_MBC.py
@@ -1,16 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 
-from math import radians
-from math import pi
 from math import degrees
-
+from math import atan
 
 ab_length = int(input())
 bc_length = int(input())
 
-ac_length = round(pow(pow(ab_length, 2) + pow(bc_length,2), 0.5))
-mc_length = ac_length / 2 
-mb_length = round(pow(pow(bc_length, 2) - pow(mc_length,2), 0.5))
-sin_radians = mc_length / bc_length
-sin_degree =  degrees(sin_radians)
+tan_radians = atan(ab_length / bc_length)
+sin_degree =  round(degrees(tan_radians))
+degree_sign = u"\N{DEGREE SIGN}"
+print(f"{sin_degree}{degree_sign}")
 
-print(f"{sin_degree}°")
